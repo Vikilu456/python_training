b687c0116e640b4232fa84648ecf991a719a323a
Victor Ilyushkin <vitterx@yandex.ru>
2020-10-31 15:01:30 +0300

    добавлен scope = "session"

--- conftest.py
+++ conftest.py
@@ -1,8 +1,7 @@
 from fixture.application import Application
 import pytest
 
-@pytest.fixture
-#(scope = "session")
+@pytest.fixture(scope = "session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
