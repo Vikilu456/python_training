f0860c5fd77be9c54d7fb222328d2c690fd4882e
Victor Ilyushkin <vitterx@yandex.ru>
2020-11-29 12:42:58 +0300

    реализован конфигурационный файл и загрузка информации из него

--- conftest.py
+++ conftest.py
@@ -1,23 +1,23 @@
 from fixture.application import Application
 import pytest
+import json
+import  os.path
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseURL")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
-    else:
-        if not fixture.is_valid():
-            pass
-        else:
-            fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or fixture.validation():
+        fixture = Application(browser=browser, base_url=target["baseURL"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -30,6 +30,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseURL", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--username", action="store", default="admin")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser, base_url, username, password):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -18,11 +18,9 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.username = username
-        self.password = password
 
 
-    def is_valid(self):
+    def validation(self):
         try:
             self.wd.current_url()
             return True
