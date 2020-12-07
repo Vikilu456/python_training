937cec0f45b5805ea9337d4e72c6db5644f5de03
Victor Ilyushkin <vitterx@yandex.ru>
2020-12-07 21:17:08 +0300

    refactor

--- model/contact.py
+++ model/contact.py
@@ -39,8 +39,7 @@ class Contact:
     #     self.homephone, self.mobilephone, self.workphone)
 
     def __eq__(self, other):
-        return (
-                           self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname, self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname, self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -13,9 +13,8 @@ def test_modify_name_data(app, db, json_contacts, check_ui):
     contact = random.choice(old_contacts)
     contact_data = json_contacts
     app.contact.modify_contact_by_id(contact_data, contact.id)
-    new_contacts = db.get_contact_list()
     assert len(old_contacts) == app.contact.count()
     old_contacts[old_contacts.index(contact)] = contact_data
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(db.get_contact_list(), key=Contact.id_or_max)
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+        assert sorted(db.get_contact_list(), key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
