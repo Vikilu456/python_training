08bfafca3be65d77bf17543bbf7ae66ff89a2cca
Victor Ilyushkin <vitterx@yandex.ru>
2020-12-09 11:15:34 +0300

    fix тест удаления из группы

--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -10,18 +10,13 @@ def test_delete_contact_to_group(app, db):
             firstname="Fname",
             lastname="lName"
         ))
-    if db.db_count_address_in_groups() == 0:
-        list_contacts = db.get_contact_list()
-        list_groups = db.get_group_list()
-        contact = random.choice(list_contacts)
-        group = random.choice(list_groups)
-        app.contact.add_contact_to_group(contact.id, group.id)
 
+    group = random.choice(db.get_group_list())
+    free_contact = random.choice(db.get_contacts_in_group(Group(id=str(group.id))))
+    if not free_contact:
+        free_contact = random.choice(db.get_contacts_not_in_group(Group(id=str(group.id))))
+        app.contact.add_contact_to_group(free_contact.id, group.id)
 
-    # получаем данные из базы для выбора random группы, в которой есть контакты
-    list_groups_from_contacts = db.get_contacts_in_group()
-    random_group = random.choice(list_groups_from_contacts)
-    # выполняем удаление random контакта, содержавшегося в random группе
-    app.contact.delete_contact_from_group(random_group.id, str(random_group.group_id))
-    assert list(filter(lambda x: x.group_id == int(random_group.group_id), db.get_contacts_in_group())) == sorted(
-        app.contact.get_contact_list_from_group_page(str(random_group.group_id)), key=Group.id_or_max)
+    app.contact.delete_contact_from_group(free_contact.id, group.id)
+    assert sorted(db.get_contacts_in_group(Group(id=str(group.id))), key=Contact.id_or_max) == sorted(
+        app.contact.get_contact_list_from_group_page(group.id), key=Contact.id_or_max)
