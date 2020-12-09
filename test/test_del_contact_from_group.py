8f7206437a89c7070af769415c2835aef76ce239
Victor Ilyushkin <vitterx@yandex.ru>
2020-12-09 11:35:36 +0300

    fix2 тест удаления из группы

--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -2,6 +2,7 @@ from model.group import Group
 from model.contact import Contact
 import random
 
+
 def test_delete_contact_to_group(app, db):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
@@ -12,11 +13,12 @@ def test_delete_contact_to_group(app, db):
         ))
 
     group = random.choice(db.get_group_list())
-    free_contact = random.choice(db.get_contacts_in_group(Group(id=str(group.id))))
-    if not free_contact:
+
+    if not db.get_contacts_in_group(Group(id=str(group.id))):
         free_contact = random.choice(db.get_contacts_not_in_group(Group(id=str(group.id))))
         app.contact.add_contact_to_group(free_contact.id, group.id)
 
+    free_contact = random.choice(db.get_contacts_in_group(Group(id=str(group.id))))
     app.contact.delete_contact_from_group(free_contact.id, group.id)
     assert sorted(db.get_contacts_in_group(Group(id=str(group.id))), key=Contact.id_or_max) == sorted(
         app.contact.get_contact_list_from_group_page(group.id), key=Contact.id_or_max)
